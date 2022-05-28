ffb917e6ed3fa1ef9165ea031453b7a7e53b589f
talliholic <talliholic@gmail.com>
2022-05-27 22:13:25 -0500

    Added forgot password functionality

--- src/routers/user.js
+++ src/routers/user.js
@@ -201,7 +201,7 @@ router.post("/reset-password/:id/:token", async (req, res, next) => {
   let { password, password2 } = req.body;
   if (password !== password2) {
     return res.send(
-      `<script>alert("Passwords do not match!");window.location.href="/${id}/${token}"</script>`
+      `<script>alert("Passwords do not match!");window.location.href="/reset-password/${id}/${token}"</script>`
     );
   }
   try {
